bdddfbc36c6ea710b52ef5293cebc4b7a252525d
Twinfox <bastian.7741@gmail.com>
2020-02-22 10:59:55 +0100

    Started implementing map creation

---
+++ Assets/Scripts/Map/Map.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public Tile[,] tiles;
+
+    private bool coordinatesAreValid(int x, int y, int z)
+    {
+        if(x >= tiles.GetLength(0))
+        {
+            Debug.LogWarning($"Trying to access x-index \"{x}\" of the map, but it is out of bounds.");
+            return false;
+        }
+
+        if(y >= tiles.GetLength(1))
+        {
+            Debug.LogWarning($"Trying to access y-index \"{y}\" of the map, but it is out of bounds.");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void ReplaceTile(int x, int y, Tile replacement) 
+    {
+        if(!coordinatesAreValid(x, y))
+        {
+            return;
+        }
+
+        RemoveTile(x, y);
+        PlaceTile(x, y, replacement);
+    }
+
+    public void RemoveTile(int x, int y)
+    {
+        if(!coordinatesAreValid(x, y))
+        {
+            return;
+        }
+
+        Destroy(tiles[x, y].gameObject);
+        tiles[x, y] = null;
+    }
+
+    public void PlaceTile(int x, int y, Tile tile)
+    {
+        if(!coordinatesAreValid(x, y))
+        {
+            return;
+        }
+
+        float xPos = (x + y) * (HexMetrics.width * 1.5f);
+        float yPos  = (x * HexMetrics.height) - (y * HexMetrics.height);
+        tiles[x, y] = tile;
+    }
+}
---
+++ Assets/Scripts/Map/Tile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameObject hoverMarker = null;
+
+    [HideInInspector]
+    public Placeable placedBuilding = null;
+
+    public bool IsOccupied
+    {
+        get
+        {
+            return placedBuilding != null;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        hoverMarker.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        hoverMarker.SetActive(false);
+    }
+
+    public void PlaceBuilding(Placeable building)
+    {
+        this.placedBuilding = building;
+        building.transform.position = transform.position;
+    }
+
+    public void Free()
+    {
+        this.placedBuilding = null;
+    }
+}
