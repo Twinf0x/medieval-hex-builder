d9b9b076329335078ddd552f868bd370e3d92a84
Twinfox <bastian.7741@gmail.com>
2020-02-23 18:31:32 +0100

    Added t3 buildings

---
+++ Assets/Scripts/Buildings/Church.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Church : Building
+{
+    public int collectionRange = 2;
+    public int bonusForMarket = -15;
+    
+    public override void Produce()
+    {
+        List<Tile> tilesInRange = locationTile.GetAllTilesAround(collectionRange);
+        int collectedFunds = baseProduction;
+
+        foreach(var tile in tilesInRange)
+        {
+            if(tile.placedBuilding == null)
+            {
+                continue;
+            }
+
+            if(tile.placedBuilding is House)
+            {
+                House house = tile.placedBuilding as House;
+                collectedFunds += house.TotalProduction;
+            }
+
+            if(tile.placedBuilding is Market)
+            {
+                Market market = tile.placedBuilding as Market;
+                collectedFunds += market.bonusForChurch;
+            }
+        }
+
+        Treasury.instance.AddMoney(collectedFunds + baseProduction);
+    }
+}
---
+++ Assets/Scripts/Buildings/Market.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Market : Building
+{
+    public int collectionRange = 2;
+    public int bonusForChurch = -15;
+
+    public override void Produce()
+    {
+        List<Tile> tilesInRange = locationTile.GetAllTilesAround(collectionRange);
+        int collectedFunds = baseProduction;
+
+        foreach(var tile in tilesInRange)
+        {
+            if(tile.placedBuilding == null)
+            {
+                continue;
+            }
+
+            if(tile.placedBuilding is House)
+            {
+                House house = tile.placedBuilding as House;
+                collectedFunds += house.TotalProduction;
+            }
+
+            if(tile.placedBuilding is Church)
+            {
+                Church church = tile.placedBuilding as Church;
+                collectedFunds += church.bonusForMarket;
+            }
+        }
+
+        Treasury.instance.AddMoney(collectedFunds + baseProduction);
+    }
+}
